3f4f6392a6d4bd26ea0ef819e223fc376be3abbf
Thelinaht <linaalturkiii@hotmail.com>
2026-03-26 17:31:00 +0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const nursesRoutes = require("./routes/nursesRoutes");
+const requestsRoutes = require("./routes/requestsRoutes");
+const approvalRoutes = require("./routes/approvalRoutes");
 
 const app = express();
 
@@ -14,6 +16,8 @@ app.get("/", (req, res) => {
 app.use("/api/nurses", nursesRoutes);
 // requests API
 app.use("/api/requests", requestsRoutes);
+// approval API
+app.use("/api/approvals", approvalRoutes);
 
 const PORT = 4000;
 
